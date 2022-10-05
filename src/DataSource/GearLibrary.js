3faba040cedf06f253cec0ed38d229459231a9b8
JayG64 <jeremie.gamache@gmail.com>
2022-10-05 12:56:15 -0400

    optimization

--- src/DataSource/GearLibrary.js
+++ src/DataSource/GearLibrary.js
@@ -1,66 +1,62 @@
 function Test_DS_Update_GearLibrary() {
-  var version = getNamedRangeValue('_Version_DataSource_Latest');
+  const version = getNamedRangeValue('_Version_DataSource_Latest');
   PropertiesService.getScriptProperties().setProperty('DataSourceUpdateVersion', version);
   DS_Update_GearLibrary();
 }
 
-var _gearLibraryTable;
-var _gearLibraryIndex;
+let _gearLibraryTable;
+let _gearLibraryIndex;
 
 function GetCostOf(material) {
   const index = _gearLibraryIndex[material];
+
   if (index == null) {
     output('error with ' + material);
     return -1;
   }
-  var cost = Number(_gearLibraryTable[index][21]);
 
-  const part1 = _gearLibraryTable[index][15];
-  const part2 = _gearLibraryTable[index][17];
-  const part3 = _gearLibraryTable[index][19];
+  let cost = Number(_gearLibraryTable[index][21]);
 
-  if (part1 != '') cost += GetCostOf(part1);
-  if (part2 != '') cost += GetCostOf(part2);
-  if (part3 != '') cost += GetCostOf(part3);
+  const parts = [_gearLibraryTable[index][15], _gearLibraryTable[index][17], _gearLibraryTable[index][19]];
+  const isEmpty = (str) => str === '';
 
-  if (part1 == '' && part2 == '' && part3 == '') cost = 0;
+  if (parts.every(isEmpty)) return 0;
+
+  parts.forEach((part) => {
+    if (!isEmpty(part)) cost += GetCostOf(part);
+  });
 
   return cost;
 }
 
 function GetAllMaterial(material, count, onlyBasicCatalyst) {
-  if (material == '') return null;
+  if (material === '') return null;
   const index = _gearLibraryIndex[material];
   if (index == null) {
     output('No index for ' + material);
     return;
   }
+  const matParts = [_gearLibraryTable[index][15], _gearLibraryTable[index][17], _gearLibraryTable[index][19]];
+  const qteParts = [
+    Number(_gearLibraryTable[index][16]),
+    Number(_gearLibraryTable[index][18]),
+    Number(_gearLibraryTable[index][20])
+  ];
 
-  const part1 = _gearLibraryTable[index][15];
-  const qtep1 = Number(_gearLibraryTable[index][16]);
+  const parts = [];
 
-  const part2 = _gearLibraryTable[index][17];
-  const qtep2 = Number(_gearLibraryTable[index][18]);
+  matParts.forEach((part, index) => {
+    const mats = GetAllMaterial(part, qteParts[index] * count);
+    if (mats != null) parts.push(mats);
+  });
 
-  const part3 = _gearLibraryTable[index][19];
-  const qtep3 = Number(_gearLibraryTable[index][20]);
-
-  var parts = [];
-  const p1 = GetAllMaterial(part1, qtep1 * count);
-  const p2 = GetAllMaterial(part2, qtep2 * count);
-  const p3 = GetAllMaterial(part3, qtep3 * count);
-
-  if (p1 != null) parts.push(p1);
-  if (p2 != null) parts.push(p2);
-  if (p3 != null) parts.push(p3);
-
-  var res = {};
+  const res = {};
   res.BasicCatalyst = 0;
   res.StatCatalyst = 0;
   res.Mats = [];
 
-  if (parts.length == 0) {
-    if (material.split('_')[1] == 'Basic') {
+  if (parts.length === 0) {
+    if (material.split('_')[1] === 'Basic') {
       res.BasicCatalyst = count;
     } else if (!onlyBasicCatalyst) {
       if (_statsTypeShort.includes(material.split('_')[1])) {
@@ -74,17 +70,17 @@ function GetAllMaterial(material, count, onlyBasicCatalyst) {
     return res;
   }
 
-  for (var i = 0; i < parts.length; i++) {
+  for (let i = 0; i < parts.length; i++) {
     const part = parts[i];
     res.BasicCatalyst += part.BasicCatalyst;
 
     if (!onlyBasicCatalyst) {
       res.StatCatalyst += part.StatCatalyst;
 
-      for (var m = 0; m < part.Mats.length; m++) {
-        var found = false;
-        for (var n = 0; n < res.Mats.length; n++) {
-          if (part.Mats[m].Name == res.Mats[n].Name) {
+      for (let m = 0; m < part.Mats.length; m++) {
+        let found = false;
+        for (let n = 0; n < res.Mats.length; n++) {
+          if (part.Mats[m].Name === res.Mats[n].Name) {
             found = true;
             res.Mats[n].Count += part.Mats[m].Count;
             break;
@@ -101,37 +97,33 @@ function GetAllMaterial(material, count, onlyBasicCatalyst) {
 }
 
 function GetAllMaterialHero(material, count) {
-  if (material == '') return null;
+  if (material === '') return null;
   const index = _gearLibraryIndex[material];
 
-  const part1 = _gearLibraryTable[index][15];
-  const qtep1 = Number(_gearLibraryTable[index][16]);
+  const matParts = [_gearLibraryTable[index][15], _gearLibraryTable[index][17], _gearLibraryTable[index][19]];
+  const qteParts = [
+    Number(_gearLibraryTable[index][16]),
+    Number(_gearLibraryTable[index][18]),
+    Number(_gearLibraryTable[index][20])
+  ];
 
-  const part2 = _gearLibraryTable[index][17];
-  const qtep2 = Number(_gearLibraryTable[index][18]);
+  const parts = [];
 
-  const part3 = _gearLibraryTable[index][19];
-  const qtep3 = Number(_gearLibraryTable[index][20]);
+  matParts.forEach((part, index) => {
+    const mats = GetAllMaterialHero(part, qteParts[index] * count);
+    if (mats != null) parts.push(mats);
+  });
 
-  var parts = [];
-  const p1 = GetAllMaterialHero(part1, qtep1 * count);
-  const p2 = GetAllMaterialHero(part2, qtep2 * count);
-  const p3 = GetAllMaterialHero(part3, qtep3 * count);
-
-  if (p1 != null) parts.push(p1);
-  if (p2 != null) parts.push(p2);
-  if (p3 != null) parts.push(p3);
-
-  var res = {};
+  const res = {};
   res.BasicCatalyst = 0;
   res.UniqueMat = null;
   res.Mats = [];
   res.CatalystMats = [];
 
-  if (parts.length == 0) {
+  if (parts.length === 0) {
     const matSplit = material.split('_');
 
-    if (matSplit[1] == 'Basic') {
+    if (matSplit[1] === 'Basic') {
       res.BasicCatalyst = count;
     } else if (_statsTypeShort.includes(matSplit[1])) {
       res.CatalystMats[0] = {};
@@ -149,7 +141,7 @@ function GetAllMaterialHero(material, count) {
     return res;
   }
 
-  for (var i = 0; i < parts.length; i++) {
+  for (let i = 0; i < parts.length; i++) {
     const part = parts[i];
     res.BasicCatalyst += part.BasicCatalyst;
 
@@ -162,10 +154,10 @@ function GetAllMaterialHero(material, count) {
       }
     }
 
-    for (var m = 0; m < part.Mats.length; m++) {
-      var found = false;
-      for (var n = 0; n < res.Mats.length; n++) {
-        if (part.Mats[m].Name == res.Mats[n].Name) {
+    for (let m = 0; m < part.Mats.length; m++) {
+      let found = false;
+      for (let n = 0; n < res.Mats.length; n++) {
+        if (part.Mats[m].Name === res.Mats[n].Name) {
           found = true;
           res.Mats[n].Count += part.Mats[m].Count;
           break;
@@ -176,10 +168,10 @@ function GetAllMaterialHero(material, count) {
       }
     }
 
-    for (var m = 0; m < part.CatalystMats.length; m++) {
-      var found = false;
-      for (var n = 0; n < res.CatalystMats.length; n++) {
-        if (part.CatalystMats[m].Name == res.CatalystMats[n].Name) {
+    for (let m = 0; m < part.CatalystMats.length; m++) {
+      let found = false;
+      for (let n = 0; n < res.CatalystMats.length; n++) {
+        if (part.CatalystMats[m].Name === res.CatalystMats[n].Name) {
           found = true;
           res.CatalystMats[n].Count += part.CatalystMats[m].Count;
           break;
@@ -203,14 +195,12 @@ function DS_Update_GearLibrary() {
   const heroesFolder = versionFolder.getFoldersByName('heroes').next();
   const file = heroesFolder.getFilesByName('M3GearLibrary.csv').next();
   const content = file.getBlob().getDataAsString();
-  const origins = ['Bio', 'Skill', 'Tech', 'Mystic', 'Mutant'];
   const extraMaterial = 6;
-
   const fileGlobal = heroesFolder.getFilesByName('M3GlobalData.json').next();
   const globalContent = fileGlobal.getBlob().getDataAsString();
   const json = JSON.parse(globalContent);
 
-  var GearTierCap = Number(json.GearTierCap);
+  let GearTierCap = Number(json.GearTierCap);
   const overrideGTC = getNamedRangeValue('Preferences_OverrideMaxGearTier');
   if (overrideGTC != '' && Number(overrideGTC) > GearTierCap) GearTierCap = Number(overrideGTC);
 
@@ -222,57 +212,54 @@ function DS_Update_GearLibrary() {
   // FORMULA
   // BIO COUNT, SKILL COUNT, TECH COUNT, MYSTIC COUNT, MUTANT COUNT
 
-  var dataStats = [];
-  var dataCraft = [];
-  var dataHero = [];
-  var dataPicture = [];
+  const dataStats = [];
+  const dataCraft = [];
+  const dataHero = [];
+  const dataPicture = [];
 
-  var statsPos = {};
-  statsPos['Health'] = 0;
-  statsPos['Damage'] = 5;
-  statsPos['Armor'] = 10;
-  statsPos['Focus'] = 15;
-  statsPos['Resist'] = 20;
+  const statsPos = {
+    Health: 0,
+    Damage: 5,
+    Armor: 10,
+    Focus: 15,
+    Resist: 20
+  };
 
-  for (var t = 0; t <= GearTierCap; t++) {
+  for (let t = 0; t <= GearTierCap; t++) {
     dataStats[t] = [];
-    for (var n = 0; n < 25; n++) {
+    for (let n = 0; n < 25; n++) {
       dataStats[t][n] = 0;
     }
   }
 
   // Easy access to each source row
   _gearLibraryIndex = new Object();
-  for (var row = 1; row < _gearLibraryTable.length; row++) {
+  for (let row = 1; row < _gearLibraryTable.length; row++) {
     _gearLibraryIndex[_gearLibraryTable[row][0]] = row;
   }
 
-  for (var o = 0; o < origins.length; o++) {
-    var cr = [origins[o], '1']; // Each origin at tier 1...
+  for (let o = 0; o < _originType.length; o++) {
+    const cr = [_originType[o], '1']; // Each origin at tier 1...
     cr.push(1); // Requires one catalyst of the gear part's stat type
-    for (var s = 0; s < _statsTypeShort.length; s++) cr.push(0); // No basic catalyst no matter what
-    for (var m = 0; m < extraMaterial; m++) cr.push('', ''); // No other material
-    for (var s = 0; s < _statsTypeShort.length; s++) cr.push('0'); // And has no cost
+    for (let s = 0; s < _statsTypeShort.length; s++) cr.push(0); // No basic catalyst no matter what
+    for (let m = 0; m < extraMaterial; m++) cr.push('', ''); // No other material
+    for (let s = 0; s < _statsTypeShort.length; s++) cr.push('0'); // And has no cost
     dataCraft.push(cr);
   }
 
   // Parse source doc
-  for (var row = 1; row < _gearLibraryTable.length; row++) {
-    var gearRow = _gearLibraryTable[row];
-    var id = gearRow[0];
+  const gearT1Ids = ['Green_Health_Mat', 'Green_Damage_Mat', 'Green_Armor_Mat', 'Green_Focus_Mat', 'Green_Resist_Mat'];
+
+  for (let row = 1; row < _gearLibraryTable.length; row++) {
+    const gearRow = _gearLibraryTable[row];
+    const id = gearRow[0];
 
     // These are the real T1
-    if (
-      id == 'Green_Health_Mat' ||
-      id == 'Green_Damage_Mat' ||
-      id == 'Green_Armor_Mat' ||
-      id == 'Green_Focus_Mat' ||
-      id == 'Green_Resist_Mat'
-    ) {
+    if (gearT1Ids.includes(id)) {
       const idPart = id.split('_');
 
       const pos = statsPos[idPart[1]];
-      for (var g = 0; g < _statsTypeShort.length; g++) {
+      for (let g = 0; g < _statsTypeShort.length; g++) {
         dataStats[1][pos + g] = gearRow[g + 2];
       }
 
@@ -291,8 +278,8 @@ function DS_Update_GearLibrary() {
       if (gearRow[15] != '') continue;
 
       // As long as the red tiers are locked, no need to take this
-      if (id.startsWith('Red_') && GearTierCap < 17) continue;
-      if (id.startsWith('White_') && GearTierCap < 21) continue;
+      if (id.startsWith('Red_') && GearTierCap < 19) continue;
+      if (id.startsWith('White_') && GearTierCap < 23) continue;
 
       dataPicture.push([gearRow[0], gearRow[14] + '.png']);
       continue;
@@ -306,17 +293,17 @@ function DS_Update_GearLibrary() {
       //for (var g = 0; g < _statsTypeShort.length; g++) st.push(gearRow[g + 2]);
       //dataStats.push(st);
       const pos = statsPos['Armor'];
-      for (var g = 0; g < _statsTypeShort.length; g++) {
+      for (let g = 0; g < _statsTypeShort.length; g++) {
         dataStats[0][pos + g] = gearRow[g + 2];
       }
 
-      for (var o = 0; o < origins.length; o++) {
-        var cr = [origins[o], '0']; // Each origin at tier 0...
+      for (let o = 0; o < _originType.length; o++) {
+        const cr = [_originType[o], '0']; // Each origin at tier 0...
         cr.push(0); // Requires no catalyst
-        for (var s = 0; s < _statsTypeShort.length; s++) cr.push(0); // No basic catalyst no matter what
+        for (let s = 0; s < _statsTypeShort.length; s++) cr.push(0); // No basic catalyst no matter what
         cr.push(id, '1'); // The only material is T1_NoArmor
-        for (var m = 1; m < extraMaterial; m++) cr.push('', ''); // No other material
-        for (var s = 0; s < _statsTypeShort.length; s++) cr.push('0'); // And has no cost
+        for (let m = 1; m < extraMaterial; m++) cr.push('', ''); // No other material
+        for (let s = 0; s < _statsTypeShort.length; s++) cr.push('0'); // And has no cost
         dataCraft.push(cr);
       }
       dataPicture.push([gearRow[0], gearRow[14] + '.png']);
@@ -324,19 +311,11 @@ function DS_Update_GearLibrary() {
     }
 
     // T1 are special case. They are all placeholders in this file
+    //const stats = ['_Focus', '_Damage', '_Resist', '_Armor', '_Health'];
     if (
       id.startsWith('T1_') &&
-      // Check both parts just in case a hero name contains one of these words
-      (id.includes('_Bio_') ||
-        id.includes('_Skill_') ||
-        id.includes('_Tech_') ||
-        id.includes('_Mystic_') ||
-        id.includes('_Mutant_')) &&
-      (id.endsWith('_Focus') ||
-        id.endsWith('_Damage') ||
-        id.endsWith('_Resist') ||
-        id.endsWith('_Armor') ||
-        id.endsWith('_Health'))
+      _originType.some((elem) => id.includes(`_${elem}_`)) &&
+      _statsTypeShort.some((elem) => id.endsWith(`_${elem}`))
     )
       continue;
 
@@ -344,8 +323,8 @@ function DS_Update_GearLibrary() {
     const tier = Number(parts[0].substring(1));
     if (tier > GearTierCap) continue;
 
-    var origin = parts[1];
-    var isHeroGear = !origins.includes(origin);
+    const origin = parts[1];
+    const isHeroGear = !_originType.includes(origin);
 
     // Hero gear is managed in another sheet
     if (isHeroGear) {
@@ -355,8 +334,8 @@ function DS_Update_GearLibrary() {
       const allMat = GetAllMaterialHero(id, 1);
 
       // Hero Gear
-      var rowHero = [heroId, tier];
-      for (var g = 0; g < _statsTypeShort.length; g++) rowHero.push(gearRow[g + 2]);
+      const rowHero = [heroId, tier];
+      for (let g = 0; g < _statsTypeShort.length; g++) rowHero.push(gearRow[g + 2]);
 
       if (allMat.CatalystMats.length + (allMat.UniqueMat == null ? 0 : 1) > 2 || allMat.CatalystMats.length > 2) {
         error('Too many material for ' + id);
@@ -370,7 +349,7 @@ function DS_Update_GearLibrary() {
       else if (allMat.UniqueMat != null) rowHero.push(allMat.UniqueMat.Name, allMat.UniqueMat.Count);
       else rowHero.push('', '');
 
-      for (var i = 0; i < 3; i++) {
+      for (let i = 0; i < 3; i++) {
         if (i < allMat.Mats.length) rowHero.push(allMat.Mats[i].Name, allMat.Mats[i].Count);
         else rowHero.push('', '');
       }
@@ -386,24 +365,24 @@ function DS_Update_GearLibrary() {
         }
       }
 
-      var stat = parts[2];
+      const stat = parts[2];
 
       // Stats are rather straight forward
-      if (origin == origins[0]) {
+      if (origin === _originType[0]) {
         //var rowStats = [stat, tier];
         //for (var g = 0; g < _statsTypeShort.length; g++) rowStats.push(gearRow[g + 2]);
         //dataStats.push(rowStats);
 
         const pos = statsPos[stat];
-        for (var g = 0; g < _statsTypeShort.length; g++) {
+        for (let g = 0; g < _statsTypeShort.length; g++) {
           dataStats[Number(tier)][pos + g] = gearRow[g + 2];
         }
       }
       // We just need one, getting the data we need for all stat type at once
-      if (stat == _statsTypeShort[0]) {
+      if (stat === _statsTypeShort[0]) {
         const allMat = GetAllMaterial(id, 1, false);
-        var rowCraft = [origin, tier, allMat.StatCatalyst, allMat.BasicCatalyst];
-        for (var s = 1; s < _statsTypeShort.length; s++) {
+        const rowCraft = [origin, tier, allMat.StatCatalyst, allMat.BasicCatalyst];
+        for (let s = 1; s < _statsTypeShort.length; s++) {
           const statMat = GetAllMaterial('T' + tier + '_' + origin + '_' + _statsTypeShort[s], 1, true);
           rowCraft.push(statMat.BasicCatalyst);
         }
@@ -412,7 +391,7 @@ function DS_Update_GearLibrary() {
           error('Too many material for ' + id);
         }
 
-        for (var i = 0; i < extraMaterial; i++) {
+        for (let i = 0; i < extraMaterial; i++) {
           if (i < allMat.Mats.length) {
             rowCraft.push(allMat.Mats[i].Name, allMat.Mats[i].Count);
           } else {
@@ -420,7 +399,7 @@ function DS_Update_GearLibrary() {
           }
         }
 
-        for (var s = 0; s < _statsTypeShort.length; s++)
+        for (let s = 0; s < _statsTypeShort.length; s++)
           rowCraft.push(GetCostOf('T' + tier + '_' + origin + '_' + _statsTypeShort[s]));
 
         dataCraft.push(rowCraft);
@@ -442,13 +421,13 @@ function DS_Update_GearLibraryCraftFormula() {
 
   const ranges = GetEmptyRows(rangeFormulas);
 
-  for (var g = 0; g < ranges.length; g++) {
-    var newData = [];
-    for (var row = ranges[g][0]; row < ranges[g][1]; row++) {
-      var newRow = [];
+  for (let g = 0; g < ranges.length; g++) {
+    const newData = [];
+    for (let row = ranges[g][0]; row < ranges[g][1]; row++) {
+      const newRow = [];
 
       // Count of gear we need of each type
-      for (var s = 0; s < _statsTypeShort.length; s++)
+      for (let s = 0; s < _statsTypeShort.length; s++)
         newRow.push(
           '=COUNTIFS(_GearTiers_Slot_' +
             _statsTypeShort[s] +
@@ -467,7 +446,7 @@ function DS_Update_GearLibraryCraftFormula() {
       newRow.push('=SUM(' + statCountCol[0] + row + ': ' + statCountCol[statCountCol.length - 1] + row + ')');
 
       // Stat catalysts
-      for (var s = 0; s < statCountCol.length; s++) newRow.push('=' + statCountCol[s] + row + ' * $C' + row);
+      for (let s = 0; s < statCountCol.length; s++) newRow.push('=' + statCountCol[s] + row + ' * $C' + row);
 
       // Basic catalyst
       newRow.push(
@@ -500,7 +479,7 @@ function DS_Update_GearLibraryCraftFormula() {
 
       // Total material
       const matCountCol = ['J', 'L', 'N', 'P', 'R', 'T'];
-      for (var s = 0; s < matCountCol.length; s++) newRow.push('=' + matCountCol[s] + row + ' * $AE' + row);
+      for (let s = 0; s < matCountCol.length; s++) newRow.push('=' + matCountCol[s] + row + ' * $AE' + row);
 
       newData.push(newRow);
     }
@@ -517,10 +496,10 @@ function DS_Update_GearLibraryHeroFormula() {
 
   const ranges = GetEmptyRows(rangeFormulas);
 
-  for (var g = 0; g < ranges.length; g++) {
-    var newData = [];
-    for (var row = ranges[g][0]; row < ranges[g][1]; row++) {
-      var newRow = [];
+  for (let g = 0; g < ranges.length; g++) {
+    const newData = [];
+    for (let row = ranges[g][0]; row < ranges[g][1]; row++) {
+      const newRow = [];
 
       newRow.push(
         '=INDEX(_GearTiers_Slot_Hero, MATCH(A' + row + ' & B' + row + ', _GearTiers_HeroId & _GearTiers_Tier, 0))'
