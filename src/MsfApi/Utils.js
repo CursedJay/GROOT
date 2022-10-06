d159930f49d81b61b5e8c5d5594de272eca30580
JayG64 <jeremie.gamache@gmail.com>
2022-10-06 00:22:33 -0400

    stuff

--- src/MsfApi/Utils.js
+++ src/MsfApi/Utils.js
@@ -5,3 +5,11 @@ function forgetMe() {
 function forgetAT() {
   GrootApi.debugForgetAccessToken();
 }
+
+function saveReqToFile(data, filename) {
+  const dateTime = Utilities.formatDate(new Date(), 'GMT+8', "yyyy-MM-dd'T'HH:mm:ss.SS");
+
+  const folder = getFolder(_zaratoolsFolder, true);
+  const file = filename ?? `msf-req-${dateTime}`;
+  folder.createFile(file, JSON.stringify(data));
+}
