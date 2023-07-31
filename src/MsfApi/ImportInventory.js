f4d3ac88e6c03e0ee21507e52fe79a303a27d980
Wilson W <wilsonjwong@outlook.com>
2023-07-31 16:45:52 -0700

    [Added] Update iso, training, and ability mats

--- src/MsfApi/ImportInventory.js
+++ src/MsfApi/ImportInventory.js
@@ -13,8 +13,8 @@ function api_importInventory(since) {
     const craftedIdsRange = `CraftedGear_Ids${column}`;
     const craftedValuesRange = `CraftedGear_Values${column}`;
 
-    updateInventoryValues(inventory.gear, matIdsRange, matValuesRange);
-    updateInventoryValues(inventory.gear, craftedIdsRange, craftedValuesRange);
+    updateInventoryValues(inventory.gear, matIdsRange, matValuesRange, 'GEAR_');
+    updateInventoryValues(inventory.gear, craftedIdsRange, craftedValuesRange, 'GEAR_');
   }
   inventoryVersionCell.setValue(inventory.since);
 }
@@ -23,16 +23,77 @@ function api_importFullInventory() {
   api_importInventory('fresh');
 }
 
-function updateInventoryValues(data, idsRange, valuesRange) {
-  const ids = getNamedRangeValues(idsRange);
-  const values = getNamedRangeValues(valuesRange);
+function updateInventoryValues(data, idsRangeName, valuesRangeName, idPrefix = '') {
+  const ids = getNamedRangeValues(idsRangeName);
+  const values = getNamedRangeValues(valuesRangeName);
 
   for (let row = 0; row < ids.length; row++) {
-    const id = `GEAR_${ids[row][0]}`;
-    if (data.hasOwnProperty(id)) values[row] = [data[id]];
+    const id = `${idPrefix}${ids[row][0]}`;
+
+    if (data.hasOwnProperty(id)) {
+      values[row] = [data[id]];
+    }
   }
 
-  setNamedRangeValues(valuesRange, values);
+  setNamedRangeValues(valuesRangeName, values);
+}
+
+function api_importIso(since) {
+  const idRanges = [];
+  const valueRanges = [];
+
+  for (let column = 1; column <= 10; column++) {
+    idRanges.push(`Inventory_Ids_Iso_${column}`);
+    valueRanges.push(`Inventory_Values_Iso_${column}`);
+  }
+  api_ImportInventoryGeneric(since, 'ISOITEM', '_Version_Iso', idRanges, valueRanges);
+}
+
+function api_importFullIso() {
+  api_importIso('fresh');
+}
+
+function api_importTraining(since) {
+  api_ImportInventoryGeneric(
+    since,
+    'CONSUMABLE',
+    '_Version_Training',
+    ['Inventory_Ids_Training'],
+    ['Inventory_Values_Training']
+  );
+}
+
+function api_importFullTraining() {
+  api_importTraining('fresh');
+}
+
+function api_importAbility(since) {
+  api_ImportInventoryGeneric(
+    since,
+    'ABILITY_MATERIAL',
+    '_Version_Ability',
+    ['Inventory_Ids_Ability'],
+    ['Inventory_Values_Ability']
+  );
+}
+
+function api_importFullAbility() {
+  api_importAbility('fresh');
+}
+
+function api_ImportInventoryGeneric(since, itemType, versionRangeName, idRangeNames, valueRangeNames) {
+  const inventoryVersionCell = getNamedRange(versionRangeName);
+  const inventoryVersionValue = inventoryVersionCell.getValue();
+  const _since = since ?? (inventoryVersionValue ? inventoryVersionValue : 'fresh');
+
+  const inventory = GrootApi.getInventoryByType(itemType, _since);
+  if (inventory === false) return;
+
+  for (let rangeCount = 0; rangeCount < idRangeNames.length; rangeCount++) {
+    updateInventoryValues(inventory.items, idRangeNames[rangeCount], valueRangeNames[rangeCount]);
+  }
+
+  inventoryVersionCell.setValue(inventory.since);
 }
 
 function saveInventoryJSON() {
